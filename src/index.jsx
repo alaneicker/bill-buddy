28d14e58e72ad423f0f7ff2d75866de3e16e6f61
Alan Eicker <alaneicker@gmail.com>
2019-09-20 09:58:30 -0500

    Moves redux provider to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app';
+
+import loginReducer from './components/login/login.reducer';
+
+const store = createStore(combineReducers({
+  loginReducer,
+}));
 
 import './assets/styles/app.scss';
 
 ReactDOM.hydrate(
-  <Router>
-    <App />
-  </Router>, 
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, 
   document.getElementById('root')
 );
