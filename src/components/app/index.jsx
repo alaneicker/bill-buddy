308c995719e7865d947077feceed23afe8309cde
Alan Eicker <alaneicker@gmail.com>
2019-09-20 13:14:16 -0500

    Fixes issues with server-side rendering

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import Splash from '../splash';
 import Login from '../login';
 
+import loginReducer from '../login/login.reducer';
+
+const store = createStore(combineReducers({
+  loginReducer,
+}));
+
 const App = () => {
   return (
-    <Switch>
+    <Provider store={store}>
       <Route path="/" exact component={Splash} />
       <Route path="/login" component={Login} />
-    </Switch>
+    </Provider>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
 import App from './components/app';
 
-import loginReducer from './components/login/login.reducer';
-
-const store = createStore(combineReducers({
-  loginReducer,
-}));
 
 import './assets/styles/app.scss';
 
 ReactDOM.hydrate(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>, 
+  <Router>
+    <App />
+  </Router>, 
   document.getElementById('root')
 );
