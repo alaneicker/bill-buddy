2ccb754f90042f46f1e474a7784595a68fb690ca
Alan Eicker <alaneicker@gmail.com>
2019-09-21 11:24:48 -0500

    Adds header nav styles

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../logo';
 import './header.scss';
 
@@ -6,6 +7,11 @@ const Header = (props) => {
   return (
     <header className="header">
       <Logo img={<img src="/assets/images/wallet.svg" alt="image of a wallet" width="36" />} text="Bill Buddy" />
+      <ul className="header__nav">
+        <li><Link to="/login">My Account</Link></li>
+        <li><Link to="/login">About</Link></li>
+        <li><Link to="/login">FAQs</Link></li>
+      </ul>
     </header>
   );
 };
