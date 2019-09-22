4bea0ee70e28da13bd8fe5d85c6021b19692bdaf
Alan Eicker <alaneicker@gmail.com>
2019-09-21 20:49:38 -0500

    refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/app';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
+import Splash from './components/splash';
+import Login from './components/login';
+
+import loginReducer from './components/login/login.reducer';
 
 import './assets/styles/app.scss';
+
+export const store = createStore(combineReducers({
+  loginReducer,
+}));
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Route path="/" exact component={Splash} />
+      <Route path="/login" component={Login} />
+    </Provider>
+  );
+};
 
 ReactDOM.hydrate(
   <Router>
